54349b83922437d98a0f1cf3e07a66016fb48860
LanNguyen <lan.nguyen@stunited.vn>
2024-11-26 14:45:45 +0700

    scroll bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import {
     Image,
     StyleSheet,
@@ -7,47 +7,77 @@ import {
     Text,
     TextInput,
     Button,
+    ScrollView,
+    Keyboard,
+    TouchableWithoutFeedback,
+    FlatList,
 } from "react-native";
 
 export default function HomeScreen() {
     const [name, setName] = useState<string>("a");
 
+    const [todoList, setTodoList] = useState([
+        { id: 1, title: "Learn react" },
+        { id: 2, title: "Learn react" },
+        { id: 3, title: "Learn react" },
+        { id: 4, title: "Learn react" },
+        { id: 6, title: "Learn react" },
+        { id: 7, title: "Learn react" },
+        { id: 8, title: "Learn react" },
+        { id: 9, title: "Learn react" },
+    ]);
+
     return (
-        <View style={styles.stepContainer}>
-            <View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.stepContainer}>
                 <Text style={styles.text}>{name}</Text>
                 <TextInput
-                    onChangeText={v => setName(v)}
+                    onChangeText={(v) => setName(v)}
                     value={name}
                     style={styles.input}
                     autoCapitalize="none"
-                    // keyboardType="numeric"
                     maxLength={12}
                     multiline
                     autoCorrect={false}
                 />
-                <Button title="ok"/>
+                <Button
+                    title="ok"
+                    color={"red"}
+                    onPress={() => alert("tap me")}
+                />
+                {/* <ScrollView style={styles.todos}>
+                    {todoList.map((item) => (
+                        <Text style={styles.text} key={item.id}>
+                            {item.title} - {item.id}
+                        </Text>
+                    ))}
+                </ScrollView> */}
+
+                <FlatList
+                    style={styles.todos}
+                    data={todoList}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <Text style={styles.text}>
+                            {item.title} - {item.id}
+                        </Text>
+                    )}
+                />
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
     stepContainer: {
         flex: 1,
-        // alignItems:"center",
-        // justifyContent:"center"
         paddingTop: 40,
         paddingHorizontal: 20,
     },
     text: {
         color: "black",
         fontSize: 40,
+        marginBottom: 40,
     },
     input: {
         borderColor: "violet",
@@ -55,5 +85,12 @@ const styles = StyleSheet.create({
         fontSize: 40,
         padding: 2,
         paddingHorizontal: 20,
+    },
+    todos: {
+        flex: 1,
+        marginTop: 20,
+        borderColor: "red",
+        borderWidth: 3,
+        padding: 10,
     },
 });
