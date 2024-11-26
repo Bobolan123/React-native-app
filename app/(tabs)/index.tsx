8c0bf14e19f95a9c114ea522bfa3643f7f3dca25
LanNguyen <lan.nguyen@stunited.vn>
2024-11-26 16:14:08 +0700

    structure add and delete todo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import InputTodo from "@/components/todos/input.todo";
+import ListTodo from "@/components/todos/list.todo";
 import React, { useState } from "react";
 import {
     Image,
@@ -14,55 +16,23 @@ import {
 } from "react-native";
 
 export default function HomeScreen() {
-    const [name, setName] = useState<string>("a");
+    const [todoList, setTodoList] = useState<ITodo[]>([]);
 
-    const [todoList, setTodoList] = useState([
-        { id: 1, title: "Learn react" },
-        { id: 2, title: "Learn react" },
-        { id: 3, title: "Learn react" },
-        { id: 4, title: "Learn react" },
-        { id: 6, title: "Learn react" },
-        { id: 7, title: "Learn react" },
-        { id: 8, title: "Learn react" },
-        { id: 9, title: "Learn react" },
-    ]);
+    const addTodo = (title:string) => {
+        var id = Date.now() + Math.random()
+        const todo = {id:id,title}
+        setTodoList([...todoList, todo])
+    } 
 
+    const deleteTodo = (id:number) => {
+        const newTodo = todoList.filter(todo=> todo.id !=id)
+        setTodoList(newTodo)
+    }
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View style={styles.stepContainer}>
-                <Text style={styles.text}>{name}</Text>
-                <TextInput
-                    onChangeText={(v) => setName(v)}
-                    value={name}
-                    style={styles.input}
-                    autoCapitalize="none"
-                    maxLength={12}
-                    multiline
-                    autoCorrect={false}
-                />
-                <Button
-                    title="ok"
-                    color={"red"}
-                    onPress={() => alert("tap me")}
-                />
-                {/* <ScrollView style={styles.todos}>
-                    {todoList.map((item) => (
-                        <Text style={styles.text} key={item.id}>
-                            {item.title} - {item.id}
-                        </Text>
-                    ))}
-                </ScrollView> */}
-
-                <FlatList
-                    style={styles.todos}
-                    data={todoList}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => (
-                        <Text style={styles.text}>
-                            {item.title} - {item.id}
-                        </Text>
-                    )}
-                />
+                <InputTodo addTodo={addTodo}/>
+                <ListTodo todoList={todoList} deleteTodo={deleteTodo}/>
             </View>
         </TouchableWithoutFeedback>
     );
@@ -71,26 +41,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     stepContainer: {
         flex: 1,
+        gap: 5,
         paddingTop: 40,
         paddingHorizontal: 20,
-    },
-    text: {
-        color: "black",
-        fontSize: 40,
-        marginBottom: 40,
-    },
-    input: {
-        borderColor: "violet",
-        borderWidth: 2,
-        fontSize: 40,
-        padding: 2,
-        paddingHorizontal: 20,
-    },
-    todos: {
-        flex: 1,
-        marginTop: 20,
-        borderColor: "red",
-        borderWidth: 3,
-        padding: 10,
     },
 });
